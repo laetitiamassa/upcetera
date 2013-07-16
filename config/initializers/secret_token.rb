cc9180f66eea2ae25ecde44ae1fdfa9f885a50a3
Laetitia Massa <laetitiamassa@gmail.com>
2013-07-16 20:14:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upcetera::Application.config.secret_token = 'bca28ac7c884e4be1d129625e74201848cade25d105eb99b563a8e8f18594b9cfe9019deb56f1b4ac325e6308c207a2b2dbea6f5db8f9c18be1591c6778dc30b'
